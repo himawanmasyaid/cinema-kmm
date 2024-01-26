6b9fb54e6f1a07113626735675cb963b67c7fc00
himawanmasyaid <himawan.masyaid@gmail.com>
2024-01-26 14:19:12 +0700

    setup library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,17 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidApplication).apply(false)
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+    alias(libs.plugins.compose).apply(false)
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,9 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 kotlin {
@@ -26,10 +29,34 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
+
+            // ktor
+            implementation("io.ktor:ktor-client-core:2.3.7")
+            implementation("io.ktor:ktor-client-logging:2.3.7")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
+            implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
+
+            // serialization
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+            // image loader
+            api("io.github.qdsfdhvh:image-loader:1.7.1")
+
+
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
+
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-okhttp:2.3.7")
+        }
+
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:2.3.7")
+            implementation("io.ktor:ktor-client-ios:2.3.7")
+        }
+
     }
 }
 
@@ -39,4 +66,7 @@ android {
     defaultConfig {
         minSdk = 28
     }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:+")
 }
